0512fdaaedea9c6a13d1f3164f94090bd5eae435
Melissa Wahnish <melissa@aarting.com>
2013-04-15 17:53:10 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storytellingapp::Application.config.secret_token = 'b8a05a9e4303e44d1a065d385318efad35bf820fb3777e3b06319cfbe4b124fcebc7e71f35ec06bfd09098822033a321adfbe34c8835c7c4d5abc0ca6772a6bc'
